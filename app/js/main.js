58dd0f13b8bc3ce65698e9585833a4170c48d4f1
Gregzorb <gregzorb@syneforge.com>
2019-04-16 17:40:46 +0300

    Lesson 16

--- app/js/main.js
+++ app/js/main.js
@@ -2,123 +2,78 @@
 * 2019-03-28
 * @gregzorb
 * Главный файл проекта
-* lesson 15. 
+* lesson 16. 
 */
 'use strict';
 
-//function showName(){
-//    console.log(this.name);
-//}
-//function showByField(field = 'name'){
-//    console.log(this[field]);
-//}
-//
-//var vasya = {name: 'Vasya', age: 40};
-//var petya = {name: 'Petya', age: 30};
-//var jessica = {name: 'Jessica', age: 20};
-//showName.call(vasya);
-//showByField.call(vasya, 'name');
+var animals = {
+    colors: 'lot'
+};
 
-//Вызов функции в другом контексте
-//Написать вызов функции, чтобы она вывела массив [1,2,3] и 'test'
-//function logArray(param1) { console.log(this); console.log(param1); }
-//var test = [1,2,3];
-//logArray.call(test, 'test');
+var cat = {
+    size: 'small',
+    name: 'Simon',
+};
+
+//cat.__proto__ = animals;
+//console.log(cat.colors);
 
 
-//Вызвать функцию таким образом, чтобы получить тест "test"
-//var obj = {testKey: 'test'};
-//function foo() {
-//    console.log(this.testKey);
-//}
-//foo.call(obj);
-
-///Вызвать функцию таким образом, чтобы получить тест "test" передавая ключ вывода
-//var obj = {testKey: 'test'};
-//function foo() {
-//    console.log(this.testKey);
-//}
-//foo.call(obj, 'testKey');
-
-//Вызвать родительский конструктор с передачей arguments
-
-//function Human(firstName, secondName) { 
-//    this.firstName = firstName; 
-//    this.secondName = secondName; 
+//function Dog(name){
+//    this.name = name;
+//    this.__proto__ = animals;
 //}
 //
-//function Student(name, surname) { 
-//    Human.apply(this, [name, surname]);
+//var dog = new Dog('Шарик');
+//console.log ( dog.colors ); //IE 10-
+
+//old style
+//function Dog(name){
+//    this.name = name;
 //}
+//Dog.prototype = animals; //только объект
+//var dog = new Dog('Шарик'); //!!Важно, что прежде всего для new
+//console.log ( dog.colors );
+
+
+
+//переопределяет только последнее
+//cat.colors = 'red';
+//console.log(animals.colors);
+//console.log(cat.colors);
+
+//console.log( cat.hasOwnProperty('colors'));//false - from animals
+//console.log( cat.hasOwnProperty('size'));//true 
+
+
+
+//встроенные объекты: Date, Array, Object
+//console.log({}.__proto__.toString);
+
+//hasOwnProperty - переопределяем toString
+//console.log( animals.toString );
+//console.log (animals.hasOwnProperty('toString') ? animals.toString : undefined );
 //
-//var student1 = new Student('Oleg', 'Vinnyk');
-//console.log (student1); // test1, test2
-//console.log (student1.firstName); // test1
-//console.log (student1.secondName); // test2
-
-
-
-/*
- * APPLY
- */
-//Math.max(1,2,3)
-//Math.max.apply(null, [1,2,3]); //null - потому что в Math не используется this, no 'use strict' - если null будет window
-//Math.max.apply(Math, [1,2,3]);
-
-//function testApply(args){
-//    this.location = 'http://google.com';
-//    console.log(this);
-//    console.log(args);
-//}
-//testApply(null, [1,2,3]);
-
-//var obj = {phrase: 'This is '};
-//var arr = ['Ivan', 'Ivanov']
+//var animals = Object.create(null);
+//animals.colors = "lot";
 //
-//function foo(firstName, lastName){
-//    console.log(this.phrase + firstName + ' ' + lastName);
-//}
-//foo.apply(obj, arr);
-
-//Bind
-//function logArray(param1) { console.log(this); console.log(param1); }
-//
-//var context = [1,2,3];
-//var logBinded = logArray.bind(context);
-//logBinded('test');
-
-//Карирование
-//способ конструирования функций, позволяющий частичное применение аргументов функции
-//var curried = function(firstName){
-//    return function(lastName){
-//        return firstName+' '+lastName;
-//    }
-//}
-//console.log(curried('Jessica')('Alba') );
-
-//try {
-//    alert(x); //no var x
-//} catch (e){
-//    console.log(e);
-//}
-//console.log('SHOW MUST GO ON');
+//console.log( animals.toString );
 
 
-//var test; 
-//try {
-//    console.log(test.test);
-//} catch (e){
-//    console.log(e);
-//}
-//console.log('SHOW MUST GO ON');
+//console.log (new Date());
+//console.log (new Date(1234567890123)); //new Date(value);
+//console.log (new Date("2019-01-01")); //new Date(dateString);
+//console.log (new Date(2019,1,1,12,0,0)); //new Date(year, month[, day[, hours[, minutes[, seconds[, milliseconds]]]]]);
+
+//console.log (Date.now());
+//console.log (Date.parse("2019-01-01"));
+
+//var d = new Date();
+//console.log ( d.getDay() ); // day of the week (0-6)
+//console.log ( d.getDate() ); // day 
+//console.log ( d.getMonth() ); //0-11
+//console.log ( d.getYear() ); //2-3 цифры согласно локали
+//console.log ( d.getFullYear() );
 
 
-/*
- * @name SomeJSDocTest
- * @param {string}
- * @return {string}
- */
-//function test(paramName) {
-//    console.log(paramName);
-//    return paramName+'.';
-//}
+
