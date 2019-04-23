9c1468293d8cc8085c5b3e6d05e0292becd051fa
Gregzorb <gregzorb@syneforge.com>
2019-04-23 17:58:24 +0300

    Дз 14

---
+++ app/js/homeworks/lesson-14.js
@@ -0,0 +1,47 @@
+/*
+* 2019-04-23
+* @gregzorb
+* Д/з
+* lesson 14. 
+*/
+'use strict';
+
+var obj = {a: 'Ananas', b: 21, c: { d: 'Oksana' }};
+//var cObj = Object.assign({}, obj);
+
+//cObj.a = 'Banan';
+//cObj.c.d = 'Vasya';
+//console.log ( obj );
+//console.log ( cObj );
+//
+//var cObj = {};
+
+
+//function copyObj(obj) {
+//    var cObj = {};
+//    for (var i in obj) {
+//        if (typeof obj[i] == 'object') {
+//            cObj[i] = copyObj(obj[i]);
+//        } else {
+//            cObj[i] = obj[i];
+//        }
+//    }
+//    return cObj;
+//}
+//
+//var obj = {a: 'Ananas', b: 21, c: { d: 'Oksana' }};
+//var dObj = copyObj(obj);
+//console.log ( obj );
+//console.log ( dObj );
+
+
+
+//var objects = [{ 'a': 1 }, { 'b': 2 }];
+// 
+//var deep = _.cloneDeep(objects);
+//console.log(objects[0]);
+//console.log(deep[0]);
+//
+//console.log(deep[0] === objects[0]);
+//console.log(deep[0] == objects[0]);
+//console.log(deep[0] === objects[0]);
