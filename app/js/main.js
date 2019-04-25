8e68c5ded1192f33981b0d97e673c75424df4a71
Gregzorb <gregzorb@syneforge.com>
2019-04-25 15:59:22 +0300

    Lesson 20

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,62 @@
 * 2019-03-28
 * @gregzorb
 * Главный файл проекта
-* lesson 19. 
+* lesson 20. 
 */
 'use strict';
 
+//console.log ( document ); // ALL -->
+//console.log ( document.documentElement ); // ONLY HTML --> 
+//console.log ( document.body ); //BODY -->
+
+//var tables = document.getElementsByTagName('table');
+//console.log(tables); //collection
+//var table = tables[0];
+//console.log(table.tHead); //only html
+//console.log(table.tBodies); //collection
+//console.log(table.tBodies[0].rows); //collection
+//console.log(table.tBodies[0].rows[0].cells); //collection
+//
+//let seconCellFirstRow = table.tBodies[0].rows[0].cells[1];
+//console.log(seconCellFirstRow); //second cell in first row collection
+//console.log(seconCellFirstRow.cellIndex); //Number
+
+
+
+
+//var blockNews = document.getElementById('block-news');
+//console.log ( blockNews.childNodes ); //NodeList, коллекция узлов
+//
+//var blockAdv = document.getElementById('block-adv'); //no text
+//console.log ( blockAdv.childNodes );
+//try {
+//    blockNews.childNodes = blockNews.childNodes;
+//} catch(e){
+//    console.error(e);
+//}
+
+//get comments
+//var events = document.getElementById('block-events');
+//[].forEach.call(events.childNodes, function(node){
+//    if ( node.nodeType == Node.COMMENT_NODE){
+//        console.log('nodeType: '+node.nodeType);
+//        console.log ( node );
+//        //previousSibling
+//        //nextSibling
+//    }
+//});
+
+
+//Не забываем про букву "s"! : getElementById (no S) || getElementsByClassName (with S)
+
+//get header only
+//var header = document.getElementsByTagName('header');
+//var events = document.getElementsByClassName('events');
+//console.log ( events ); 
+//console.log ( events[0].querySelector('div') ); 
+//console.log ( events[0].querySelectorAll('div.events') ); 
+
+//contains
+//var eventBlock = document.getElementById('block-events');
+//var event1 = document.getElementById('event-1');
+//console.log (eventBlock.contains(event1));
---
+++ app/js/table.js
@@ -0,0 +1,41 @@
+/*
+* 2019-04-25
+* @gregzorb
+* Для урока 20. 
+*/
+'use strict';
+
+
+////Все элементы label внутри таблицы. Должно быть 3 элемента.
+//var table = document.getElementsByTagName('table')[0];
+//var labels = table.getElementsByTagName('label');
+//
+////Первую ячейку таблицы (со словом "Возраст")
+//var cells = table.getElementsByTagName('td');
+//[].forEach.call(cells, function(cell){
+//    console.log(cell.innerHTML);
+//    //todo: return index
+//});
+
+
+//Вторую форму в документе.
+//Форму с именем search, без использования её позиции в документе.
+//Элемент input в форме с именем search. Если их несколько, то нужен первый.
+//Элемент с именем info[0], без точного знания его позиции в документе.
+//Элемент с именем info[0], внутри формы с именем search-person.
+
+
+
+//var ageInputs = document.getElementsByName("age");
+//for(let i = 0; i < ageInputs.length; ++i){
+//    console.log( ageInputs[i].attributes );
+//    console.log( ageInputs[i].hasAttribute('value') );
+//    console.log( ageInputs[i].getAttribute('value') );
+//
+//    ageInputs[i].setAttribute('value', 100);
+//    console.log( ageInputs[i].getAttribute('value') );
+//
+//    ageInputs[i].removeAttribute('value');
+//    console.log( ageInputs[i].hasAttribute('value') );
+//}
+
